e39eecd191b75355ac3c5cbc6e402e156dfe3355
pxzone <158117360+pxzone@users.noreply.github.com>
2024-02-11 22:06:26 +0800

    Update config.php

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/pxzone/';
+$config['base_url'] = 'http://localhost/pxzone.online/';
 // $config['base_url'] = 'https://pxzone.online/';
 
 /*
