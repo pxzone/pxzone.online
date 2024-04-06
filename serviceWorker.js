c218470b8eed671bd9102cef225419081797af49
pxzone <158117360+pxzone@users.noreply.github.com>
2024-04-06 22:22:44 +0800

    Update serviceWorker.js

--- serviceWorker.js
+++ serviceWorker.js
@@ -20,10 +20,3 @@ self.addEventListener("install", installEvent => {
   )
 })
 
-self.addEventListener("fetch", fetchEvent => {
-  fetchEvent.respondWith(
-    caches.match(fetchEvent.request).then(res => {
-      return res || fetch(fetchEvent.request)
-    })
-  )
-})
