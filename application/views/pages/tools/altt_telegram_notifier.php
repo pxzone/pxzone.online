186b0687b6eb922f8a7ab6de553f25c2c6195637
pxzone <158117360+pxzone@users.noreply.github.com>
2024-04-06 22:22:27 +0800

    Update altt_telegram_notifier.php

--- application/views/pages/tools/altt_telegram_notifier.php
+++ application/views/pages/tools/altt_telegram_notifier.php
@@ -53,30 +53,37 @@
                             <div class="card card-anchor bg-dark pt-2 pb-2 padding-right-30 padding-left-30">
                                
                                 <div class="mt-3 mb-3">
-                                <img width="100" src="https://i.ibb.co/D7n9vQM/Altcoinstalks-Notifier1.png" alt="AltcoinsTalks Telegram Notifier">
+                                <img width="180" src="https://pxzone.online/assets/images/other/altt-tg-bot.webp" alt="AltcoinsTalks Telegram Notifier">
 
-                                <h2>Telegram bot:</h2>
-                                <a href="https://t.me/altt_notifier_bot">@altt_notifier_bot</a>
+                                <h2 class="font-20 mt-3">Telegram bot:</h2>
+                                <a href="https://t.me/altt_notifier_bot"> <i class="uil uil-telegram font-20"></i> @altt_notifier_bot</a>
 
-                                <h2>Features</h2>
+                                <h2 class="font-20 mt-3">Features</h2>
                                 <ul>
                                     <li>Mention/quote notification</li>
                                     <li>Track Phrase</li>
                                     <li>Track user posts</li>
-                                    <li>Ignore user posts</li>
+                                    <li>Track boards</li>
+                                    <li>Track replies in a topic/thread</li>
+                                    <li>Ignore users</li>
+                                    <li>Karma notification</li>
                                     <li>Stop notification/unsubscribe</li>
                                 </ul>
 
-                                <h2>Command:</h2>
+                                <h2 class="font-20 mt-3">Command:</h2>
                                 <ul>
                                     <li><b>/start</b> - to start the bot, can be seen on the first visit the bot.</li>
                                     <li><b>/menu</b> - shows unsubscribe and track phrase</li>
                                 </ul>
+                                
+                                <h2 class="font-20">Screenshots</h2>
+                                <p class="mt-3"><strong><img src="https://i.ibb.co/YLb4qg4/1000002339.gif" alt="" /></strong></p>
+                                
+                                <p class="mt-3"><strong><img height="203" src="https://i.ibb.co/ss5k0tj/15856356.png" /></strong></p>
+                                <p class="mt-3"><img src="https://pxzone.online/api/telegram/users/_count" alt="" width="260" height="17" /></p>
 
                                 <p class="mt-3">
-                                The bot scrapes data per msg_id in 1-minute intervals, meaning the notification might be delayed by 1 minute or more. 
-                                The interval is due to the cron job's limit. The delay might be more painful if the site's server is down. 
-                                The interval time will be changed in the future so in the meantime..
+                                The bot scrapes data per <a href="https://www.altcoinstalks.com/index.php?action=recent" target="_blank">action=recent </a>first and second page in 1-minute interval using cron job.
                                 </p>
 
                                 AltcoinsTalks Thread: <a href="https://www.altcoinstalks.com/index.php?topic=315728.0" target="_blank" rel="nopener nofollow">Here...</a>
