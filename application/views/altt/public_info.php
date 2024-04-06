b269e79c91f68266efe7088e94d560f9c76bda43
pxzone <158117360+pxzone@users.noreply.github.com>
2024-04-06 09:23:14 +0800

    Updated allt public info view

---
+++ application/views/altt/public_info.php
@@ -0,0 +1,100 @@
+    <div id="_web_container" class="dark-theme">
+            <div class="other-section padding-bottom-30 c-dwhite"  >
+                <div class="first-section padding-bottom-30 c-dwhite">
+                    <div class="container">
+                        <div class="text-center">
+                            <h1 class="f-h1 mb-2 c-white">AltcoinsTalks' Statistics Overview</h1>
+                            <p class="p-text c-white font-15"></p>
+                        </div>
+                    </div>
+                </div>
+                <div class="container-fluid">
+                    <div class="row">
+                        <div class="col-lg-2 col-6">
+                            <div class="card card-anchor bg-dark c-white pt-2 pb-3 padding-right-30 padding-left-30">
+                                <h2 class="font-15">Posts 24h</h2>
+                                <h4 class="font-14" id="posts_24h">0</h4>
+                            </div>
+                        </div>
+                        <div class="col-lg-2 col-6">
+                            <div class="card card-anchor bg-dark c-white pt-2 pb-3 padding-right-30 padding-left-30">
+                                <h2 class="font-15">Karma 24h</h2>
+                                <h4 class="font-14" id="karma_24h">0</h4>
+                            </div>
+                        </div>
+                        <div class="col-lg-2 col-6">
+                            <div class="card card-anchor bg-dark c-white pt-2 pb-3 padding-right-30 padding-left-30">
+                                <h2 class="font-15">Parsed Users</h2>
+                                <h4 class="font-14" id="parsed_users_all">0</h4>
+                            </div>
+                        </div>
+                        <div class="col-lg-2 col-6">
+                            <div class="card card-anchor bg-dark c-white pt-2 pb-3 padding-right-30 padding-left-30">
+                                <h2 class="font-15">Parsed Posts</h2>
+                                <h4 class="font-14" id="archive_posts_all">0</h4>
+                            </div>
+                        </div>
+
+                        <div class="col-lg-2 col-6">
+                            <div class="card card-anchor bg-dark c-white pt-2 pb-3 padding-right-30 padding-left-30">
+                                <h2 class="font-15">Parsed Topics</h2>
+                                <h4 class="font-14" id="archive_topic_all">0</h4>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-lg-6">
+                            <div class="card card-anchor bg-dark c-white pt-2 pb-3 padding-right-20 padding-left-20">
+                                <h2 class="font-15">Posts per day</h2>
+                                <div dir="ltr">
+                                    <canvas id="posts_per_day" class="apex-charts mt-3"></canvas>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="col-lg-6">
+                            <div class="card card-anchor bg-dark c-white pt-2 pb-3 padding-right-20 padding-left-20">
+                                <h2 class="font-15">Topics per day</h2>
+                                <div dir="ltr">
+                                    <canvas id="topics_per_day" class="apex-charts mt-3"></canvas>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <!-- <div class="row">
+                        <div class="col-lg-6 col-12">
+                            <div class="card card-anchor bg-dark c-white pt-2 pb-3 padding-right-20 padding-left-20">
+                                <h2 class="font-15">Top 20 Posters</h2>
+                                <div id="top_posters" class="font-13">
+
+                                    <div class="d-flex flex-row">
+                                        <div class="p-2 flex-fill">
+                                            <label>Username</label>
+                                        </div>
+                                        <div class=" p-2 flex-fill">
+                                            <div class="progress mt-1">
+                                                <div class="progress-bar bg-warning" role="progressbar" style="width: 25%;" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100">25</div>
+                                            </div>
+                                        </div>
+                                    </div> 
+                                                
+                                </div>
+                            </div>
+                        </div>
+                         -->
+                     
+
+                        <!-- <div class="col-lg-6">
+                            <div class="card card-anchor bg-dark c-white pt-2 pb-5 padding-right-20 padding-left-20">
+                                <h2 class="font-15">Top 10 Boards</h2>
+                                <div id="top_boards" class="font-13">
+                                </div>
+                            </div>
+                        </div> -->
+                    </div>
+                    
+                    <div class="back-to-top cursor-pointer"><i class="uil uil-angle-up"></i></div>
+                </div>
+            </div> 
+        </div>
